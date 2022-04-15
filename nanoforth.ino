ede5b0d1ec78fdeffc34fa2737b29c86ff5b3679
Ed Paradis <eparadis@users.noreply.github.com>
2022-04-14 19:54:52 -0700

    replace blinky LED example with a counting 8 digit 7-segment display

--- nanoforth.ino
+++ nanoforth.ino
@@ -15,28 +15,71 @@
  *  + ok prompt
  *  + try type WRD and hit return on the input above
  */
-#include "nanoforth.h"
+#include <Arduino.h>
+#include <AceTMI.h> // SimpleTmi1638Interface
+#include <AceSegment.h> // Tm1638Module
+#include <AceSegmentWriter.h> // NumberWriter
 
-N4_TASK(blink)                    ///< create blinking task (i.e. built-in LED on pin 13)
+using ace_tmi::SimpleTmi1638Interface;
+using ace_segment::LedModule;
+using ace_segment::Tm1638AnodeModule;
+using ace_segment::NumberWriter;
+
+// Replace these with the PIN numbers of your dev board.
+// The TM1638 protocol is very similar to SPI, so I often use the SPI pins.
+const uint8_t CLK_PIN = 0;
+const uint8_t DIO_PIN = 1;
+const uint8_t STB_PIN = 2;
+const uint8_t NUM_DIGITS = 8;
+
+// My TM1638 LED module contains no filtering capacitor on the DIO, CLK, and STB
+// lines. So it should support the highest clock frequency using a 1 micro
+// transition delay.
+const uint8_t DELAY_MICROS = 1;
+
+using TmiInterface = SimpleTmi1638Interface;
+TmiInterface tmiInterface(DIO_PIN, CLK_PIN, STB_PIN, DELAY_MICROS);
+Tm1638AnodeModule<TmiInterface, NUM_DIGITS> ledModule(tmiInterface);
+
+NumberWriter<LedModule> numberWriter(ledModule);
+
+#include "src/nanoforth.h"
+
+//N4_TASK(blink)                    ///< create blinking task (i.e. built-in LED on pin 13)
+//{
+//    digitalWrite(LED_BUILTIN, HIGH);
+//    N4_DELAY(500);
+//    digitalWrite(LED_BUILTIN, LOW);
+//    N4_DELAY(500);
+//}
+//N4_END;
+
+N4_TASK(display_count)
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    N4_DELAY(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    N4_DELAY(500);
+  numberWriter.writeUnsignedDecimalAt(0, millis()/1000, 8);
+  ledModule.flush();
+  N4_DELAY(1000);
 }
 N4_END;
 
 NanoForth n4;                     ///< create NanoForth instance
 void setup()
 {
+    tmiInterface.begin();
+    ledModule.begin();
+  
     Serial.begin(115200);         ///< init Serial IO, make sure it is set to 'Both BL & CR' to capture input
-
     if (n4.begin()) {             /// default: (Serial,0x480,0x80), try reducing if memory is constrained
         Serial.print(F("ERROR: memory allocation failed!"));
     }
-    n4.add(blink);                ///< add blink task to NanoForth task manager
 
-    pinMode(LED_BUILTIN, OUTPUT);
+    numberWriter.writeHexChars2At(3, 0xE, 0xD);
+    ledModule.setBrightness(2);
+    ledModule.flush();
+    delay(500);
+
+    n4.add(display_count);
+//    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
